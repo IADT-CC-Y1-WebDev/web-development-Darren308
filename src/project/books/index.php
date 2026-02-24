459bb5ab1ffc079017502ba724a0a5017b812215
Darren Carroll <n00255308@iadt.ie>
2026-02-24 13:59:29 +0000

    starting the form

---
+++ src/project/books/index.php
@@ -0,0 +1,21 @@
+<?php
+require_once './php/lib/config.php';
+?>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <?php include './php/inc/head_content.php'; ?>
+        <title>Books</title>
+    </head>
+    <body>
+        <div class="container">
+            <div class="width-12 header">
+                <?php require './php/inc/flash_message.php'; ?>
+            </div>
+            <div class="width-12 header">
+                <p><a href="book_list.php">View Books</a></p>
+            </div>
+        </div>
+    </body>
+</html>
+                        
---
+++ src/project/books/php/classes/Book.php
@@ -0,0 +1,50 @@
+<?php
+class Book {
+    public $id;
+    public $title;
+    public $author;
+    public $publisher_id;
+    public $year;
+    public $isbn;
+    public $description;
+    public $cover_filename;
+    private $db;
+
+    public function __construct($data = []) {
+        $this->db = DB::getInstance()->getConnection();
+
+        if (!empty($data)) {
+            $this->id = $data['id'] ?? null;
+            $this->title = $data['title'] ?? null;
+            $this->author = $data['author'] ?? null;
+            $this->publisher_id = $data['publisher_id'] ?? null;
+            $this->year = $data['year'] ?? null;
+            $this->isbn = $data['isbn'] ?? null;
+            $this->description = $data['description'] ?? null;
+            $this->cover_filename = $data['cover_filename'] ?? null;
+        }
+    }
+
+    public static function findAll() {
+        $db = DB::getInstance()->getConnection();
+        $stmt = $db->prepare("SELECT * FROM books ORDER BY title");
+        $stmt->execute();
+
+        $books = [];
+        while ($row = $stmt->fetch()) {
+            $books[] = new Book($row);
+        }
+        return $books;
+    }
+
+    public static function findById($id) {
+        $db = DB::getInstance()->getConnection();
+        $stmt = $db->prepare("SELECT * FROM books WHERE id = :id");
+        $stmt->execute(['id' => $id]);
+
+        $row = $stmt->fetch();
+        if ($row) {
+            return new Book($row);
+        }
+    }
+}
